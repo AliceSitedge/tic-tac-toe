b883d2dfdee30522b5fe55a0f8d6d72f975ba810
AntonyMo <32247865+antonymoes@users.noreply.github.com>
2019-02-04 22:03:41 +0300

    algorithm added

--- include/map.h
+++ include/map.h
@@ -2,6 +2,7 @@
 #define TIC_TAC_TOE_MAP_H
 
 #include <array>
+#include <SFML/System/Vector2.hpp>
 
 enum cell {
     EMPTY = 0,
@@ -10,17 +11,19 @@ enum cell {
 };
 
 class Map {
-public:
+  public:
     Map();
     void make_a_move(int16_t y, int16_t x, cell what);
     bool is_this_a_win(int16_t y, int16_t x, cell what);
-    bool is_empty(int16_t y, int16_t x);
-    int get_size();
-    cell get_cell(int16_t y, int16_t x);
-    void draw();
-private:
+    bool is_empty(int16_t y, int16_t x) const;
+    int get_size() const;
+    cell get_cell(int16_t y, int16_t x) const;
+    void draw() const;
+  private:
     const static int16_t size = 20;
+    const static int16_t win_num = 5; 
     std::array<std::array<cell, size>, size> field;
+    int row_len_in_direction (sf::Vector2i where_from, sf::Vector2i dir, cell what) const;
 };
 
 #endif //TIC_TAC_TOE_MAP_H
--- src/map.cpp
+++ src/map.cpp
@@ -1,6 +1,7 @@
 #include <map.h>
 #include <iostream>
 #include <iomanip>
+#include <functional>
 
 Map::Map() {
     for (auto &row : field) {
@@ -12,22 +13,69 @@ Map::Map() {
 
 bool Map::is_this_a_win(int16_t y, int16_t x, cell what) {
     field[y][x] = what;
+
+    if (row_len_in_direction(sf::Vector2i(x, y), sf::Vector2i(1, 0), what) +
+        row_len_in_direction(sf::Vector2i(x, y), sf::Vector2i(-1, 0), what) >= win_num - 1) {
+        return true;
+    }
+
+    if (row_len_in_direction(sf::Vector2i(x, y), sf::Vector2i(0, 1), what) +
+        row_len_in_direction(sf::Vector2i(x, y), sf::Vector2i(0, -1), what) >= win_num - 1) {
+        return true;
+    }
+
+    if (row_len_in_direction(sf::Vector2i(x, y), sf::Vector2i(1, 1), what) +
+        row_len_in_direction(sf::Vector2i(x, y), sf::Vector2i(-1, -1), what) >= win_num - 1) {
+        return true;
+    }
+
+    if (row_len_in_direction(sf::Vector2i(x, y), sf::Vector2i(-1, 1), what) +
+        row_len_in_direction(sf::Vector2i(x, y), sf::Vector2i(1, -1), what) >= win_num - 1) {
+        return true;
+    }
+
+    /*row_length = 1;
+    i = x + 1;
+    while (i <= size && field[i][y] == what) {
+        row_length++;
+        i++;
+    }
+    i = x - 1;
+    while (i >= 0 && field[i][y] == what) {
+        row_length++;
+        i--;
+    }
+    if (row_length == 5) {
+        return true;
+    }
+
+    row_length = 1;
+    j = y + 1;
+    while (j <= size && field[x][j] == what) {
+        row_length++;
+        j++;
+    }
+    j = y - 1;
+    while (j >= 0 && field[x][j] == what) {
+        row_length++;
+        j--;
+    }*/
     return false;
 }
 
-bool Map::is_empty(int16_t y, int16_t x) {
+bool Map::is_empty(int16_t y, int16_t x) const {
     return field[y][x] == EMPTY;
 }
 
-int Map::get_size() {
+int Map::get_size() const {
     return size;
 }
 
-cell Map::get_cell(int16_t y, int16_t x) {
+cell Map::get_cell(int16_t y, int16_t x) const {
     return field[y][x];
 }
 
-void Map::draw() {
+void Map::draw() const {
     const int width = 4;
 
     std::cout << std::setw(width) << "";
@@ -52,4 +100,18 @@ void Map::draw() {
 
 void Map::make_a_move(int16_t y, int16_t x, cell what) {
     field[y][x] = what;
+}
+
+int Map::row_len_in_direction(sf::Vector2i where_from, sf::Vector2i dir, cell what) const {
+    int next_x = where_from.x + dir.x;
+    int next_y = where_from.y + dir.y;
+    if (next_x < 0 || next_x >= size || next_y < 0 || next_y >= size) {
+        return 0;
+    }
+
+    if (field[next_x][next_y] != what) {
+        return 0;
+    } else {
+        return 1 + row_len_in_direction(where_from + dir, dir, what);
+    }
 }
